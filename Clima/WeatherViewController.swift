9fdca79b51a7953e30e3ec9539c737005d9e11c0
mzbanez <mzbanez@gmail.com>
2017-08-05 11:17:01 +0900

    new updates

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -12,7 +12,9 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    //let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    
+    let APP_ID = MyOpenWeatherApiKey
     
 
     //TODO: Declare instance variables here
